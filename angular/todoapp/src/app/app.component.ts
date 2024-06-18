fbeb900ba699ab6a7c13a73eec157f1a6f83088a
jisumov <jisumov@gmail.com>
2024-06-18 16:30:23 -0500

    feat: add initial layout
    - Remove basic syntax from app.component files
    - Add template into home.component files
    - Add labs.component files for testing

--- angular/todoapp/src/app/app.component.ts
+++ angular/todoapp/src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  welcome = 'Hola!';
-  tasks = [
-    'Instalar el Angular CLI',
-    'Crear proyecto',
-    'Crear componentes',
-  ]
+
 }
