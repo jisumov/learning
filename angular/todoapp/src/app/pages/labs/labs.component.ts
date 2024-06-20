7663d5835d6d57a5163838e4ce0d68c5f29bee1d
jisumov <jisumov@gmail.com>
2024-06-20 18:24:48 -0500

    feat: add keydown examples

--- angular/todoapp/src/app/pages/labs/labs.component.ts
+++ angular/todoapp/src/app/pages/labs/labs.component.ts
@@ -33,4 +33,9 @@ export class LabsComponent {
   changeHandler(event: Event) {
     console.log(event);
   }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
