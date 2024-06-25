09c039e6c8e4844f5f10ca5a45cc22ed0ce6a946
jisumov <jisumov@gmail.com>
2024-06-25 17:04:24 -0500

    feat: add purchase service and controller

--- spring/platzi-market/src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
+++ spring/platzi-market/src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
@@ -2,6 +2,7 @@ package com.platzi.market.persistence.mapper;
 
 import com.platzi.market.domain.PurchaseItem;
 import com.platzi.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -15,6 +16,7 @@ public interface PurchaseItemMapper {
     })
     PurchaseItem toPurchaseItem(ComprasProducto producto);
 
+    @InheritInverseConfiguration
     @Mappings({
             @Mapping(target = "compra", ignore = true),
             @Mapping(target = "producto", ignore = true),
